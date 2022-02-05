050438ab929333d08493214672406f877f1c8402
SuperCat <arahmanekici@hotmail.com>
2022-02-05 15:07:38 +0300

    First Commit

---
+++ src/SwitchCase.java
@@ -0,0 +1,21 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        int day= 8;
+        switch(day) {
+            case 1:
+                System.out.println("Pzt");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Çrş");
+                break;
+            default:
+                System.out.println("Pzt Salı Çrş Değildir");
+
+
+        }
+    }
+}
